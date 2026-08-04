ead0496d73c9f34be26867171e3319b188d62bb8
Gourab550 <sahagourabnda@gmail.com>
2026-08-04 10:08:39 +0000

    conversion done

---
+++ 01_basics/conversion_operation.js
@@ -0,0 +1,30 @@
+let score=null
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueInNumber= Number(score)
+// console.log(typeof score)
+// console.log(valueInNumber)
+
+/*
+"33" => 33
+"33abc" => NaN
+true => 1
+false => 0
+null => 0
+undefined => NaN
+*/
+// Boolean conversion
+let isLoggedIn= "gourab"
+let booleanisLoggedIn= Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn)
+
+/*
+1=> true; 0=> false;
+""=> false; "Gourab"=>true
+*/
+// String conversion
+let someNumber=77
+let stringNumber= String (someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
