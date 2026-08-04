3150897fcfcba665c9e84c2276803ca943ce3ee4
Gourab550 <sahagourabnda@gmail.com>
2026-08-04 09:06:31 +0000

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,10 @@
+const accountID= 12345;
+let accountEmail="Gourab@gmail.com"
+var accountPassword="1245678"
+accountCity="kolkata"
+
+console.log(accountID)
+
+/* prefer not to use var because of scope and functional issues */
+
+console.table([accountID, accountEmail, accountPassword, accountCity])
