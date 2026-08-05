fb28ebc6e5e97f7f3aaea13449b957a804cf34f0
Gourab550 <sahagourabnda@gmail.com>
2026-08-05 11:05:38 +0000

    string

---
+++ 01_basics/string.js
@@ -0,0 +1,42 @@
+let name=" Gourab saha"
+let repoCount= 50
+
+// console.log(name + repoCount + "value")
+
+console.log(`hello my name is ${name} and my repo is ${repoCount}`)
+
+const name = "Gourab"
+const repoCount = 30
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('gourab-gs-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   gourab    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://google%20you.com"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('saha'))
+
+console.log(gameName.split('-'));
